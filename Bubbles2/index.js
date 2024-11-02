84b1e4d7ec856a7c27932300c25434e439f848d1
Qlex-cmos <alexcheula@gmail.com>
2024-11-02 23:46:11 +0100

    injection classe + appendChild

--- Bubbles2/index.js
+++ Bubbles2/index.js
@@ -1,3 +1,7 @@
 const bubble = document.createElement("span");
 bubble.classList.add("bubble");
 document.body.appendChild(bubble);
+
+const size = Math.random() * 100 + 100 + "px";
+bubble.style.height = size;
+bubble.style.width = size;
