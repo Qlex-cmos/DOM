dedae488a1cb2ab3218e896057a10c5024b73a1b
Qlex-cmos <alexcheula@gmail.com>
2024-11-04 00:26:30 +0100

    setTimeout, setInterval, textContent

--- Bubbles2/index.js
+++ Bubbles2/index.js
@@ -1,13 +1,30 @@
-const bubble = document.createElement("span");
-bubble.classList.add("bubble"); // injection d'une classe
-document.body.appendChild(bubble); // injecter le span créé dans le body
+const counterDisplay = document.querySelector("h3");
+let counter = 0;
 
-// Randomiser la taille des bulles
-const size = Math.random() * 100 + 100 + "px";
-bubble.style.height = size; // on injecte la variable en pointant l'élément
-bubble.style.width = size; // on injecte la variable en pointant l'élément
+const bubbleMaker = () => {
+  const bubble = document.createElement("span");
+  bubble.classList.add("bubble"); // injection d'une classe
+  document.body.appendChild(bubble); // injecter le span créé dans le body
 
-bubble.style.top = Math.random() * 100 + 50 + "%";
-bubble.style.left = Math.random() * 100 + "%";
+  // Randomiser la taille des bulles
+  const size = Math.random() * 100 + 100 + "px";
+  bubble.style.height = size; // on injecte la variable en pointant l'élément
+  bubble.style.width = size; // on injecte la variable en pointant l'élément
 
-bubble.style.setProperty("--left", Math.random() * 100 + "%");
+  bubble.style.top = Math.random() * 100 + 50 + "%";
+  bubble.style.left = Math.random() * 100 + "%";
+
+  bubble.style.setProperty("--left", Math.random() * 100 + "%");
+
+  bubble.addEventListener("click", () => {
+    counter++;
+    counterDisplay.textContent = counter;
+    bubble.remove();
+  });
+
+  setTimeout(() => {
+    bubble.remove();
+  }, 8000);
+};
+
+setInterval(bubbleMaker, 300);
