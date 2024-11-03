bd8de7d7f4a2604397c21012bfa9c37053bca397
Qlex-cmos <alexcheula@gmail.com>
2024-11-03 23:48:14 +0100

    Math.random(), @keyframes, setProperty

--- Bubbles2/index.js
+++ Bubbles2/index.js
@@ -1,7 +1,13 @@
 const bubble = document.createElement("span");
-bubble.classList.add("bubble");
-document.body.appendChild(bubble);
+bubble.classList.add("bubble"); // injection d'une classe
+document.body.appendChild(bubble); // injecter le span créé dans le body
 
+// Randomiser la taille des bulles
 const size = Math.random() * 100 + 100 + "px";
-bubble.style.height = size;
-bubble.style.width = size;
+bubble.style.height = size; // on injecte la variable en pointant l'élément
+bubble.style.width = size; // on injecte la variable en pointant l'élément
+
+bubble.style.top = Math.random() * 100 + 50 + "%";
+bubble.style.left = Math.random() * 100 + "%";
+
+bubble.style.setProperty("--left", Math.random() * 100 + "%");
